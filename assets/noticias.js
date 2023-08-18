6a73d8c45578759592976c4fb40067822f39bfca
Olstertecn11 <otzunund@miumg.edu.gt>
2023-08-18 16:22:51 -0600

    adding modal#

--- assets/noticias.js
+++ assets/noticias.js
@@ -6,6 +6,7 @@ const contenido = document.getElementById('contenido');
 const titulo = document.getElementById('titulo');
 var nom = 0;
 var per = 0;
+let all_news = "";
 
 
 function saveInfo() {
@@ -75,8 +76,84 @@ function changeFormState() {
 }
 
 
-function loadNews() {
 
+function loadNews() {
+  var mytable = document.getElementById("news_table");
+  $.ajax({
+    url: 'php/noticias/obtenerNoticias.php',
+    type: 'POST',
+    async: false,
+    success: function(data) {
+      all_news = JSON.parse(data);
+      $.each(JSON.parse(data), (key, item) => {
+        mytable.innerHTML += `
+            <tr>
+              <th scope="row">${key}</th>
+              <td>${item.titulo}</td>
+              <td><img class="row-img" src="./img/Noticias/${item.imagen}" alt=""></td>
+              <td>${item.estado}</td>
+              <td>${item.fecha}</td>
+              <td>
+                <button class="btn btn-primary" onclick="openModal(${item.id})" data-micromodal-trigger>Visualizar</button>
+              </td>
+            </tr>
+        `
+      })
+    },
+    error: function(jqXHR, error) {
+      console.log(error);
+    }
+  });
 }
+
+
+loadNews();
+
+function openModal(myid) {
+  const item = all_news.filter((el) => el.id == myid ? el : '');
+
+
+  var modal = createModal();
+  modal.open();
+  modal.setContent(`<h1>${item[0].titulo}</h1>
+  <textarea>${item[0].contenido}</textarea>
+`);
+
+  // modal.setContent(``);
+
+  modal.addFooterBtn('Guardar cambios', 'tingle-btn tingle-btn--primary', function() {
+    modal.close();
+  });
+
+  modal.addFooterBtn('Salir sin guardar', 'tingle-btn tingle-btn--danger', function() {
+    modal.close();
+  });
+}
+
+function createModal() {
+  return new tingle.modal({
+    footer: true,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+    cssClass: ['custom-class-1', 'custom-class-2'],
+    onOpen: function() {
+      console.log('modal open');
+    },
+    onClose: function() {
+      console.log('modal closed');
+    },
+    beforeClose: function() {
+      // here's goes some logic
+      // e.g. save content before closing the modal
+      return true; // close the modal
+      return false; // nothing happens
+    }
+  });
+}
+
+
+
+
 
 
